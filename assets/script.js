ce370cd880671fe30c9ece242b6fde671184c735
elReclusa <howell_j@hotmail.com>
2022-10-27 19:23:16 -0500

    Added edited code for limiting to top headlines, commented on leaving space for city name var

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-fetch(`https://newsapi.org/v2/everything?q=kansas+city&apiKey=331040a253684eed8346e5eb64aabfe0`)
+//fetch(`https://newsapi.org/v2/everything?q=kansas+city&apiKey=331040a253684eed8346e5eb64aabfe0`) for all news
+fetch(`https://newsapi.org/v2/top-headlines?q=kansas+city&apiKey=331040a253684eed8346e5eb64aabfe0`) //for top headlines
+//replace "kansas+city" with " + var name for city input + "&apikey...
             .then((response) => response.json())
             .then(data => console.log(data)) 
            
