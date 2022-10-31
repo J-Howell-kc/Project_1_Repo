c36ceccc9ea8d5ed1d62f8a2ba7d5d10e10cbbc7
elReclusa <howell_j@hotmail.com>
2022-10-31 13:27:03 -0500

    tutor helped fix newsapi issue. Working on my end at the moment.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
+const searchItem2=document.getElementById("searchItem2")
+
+
+
 //fetch(`https://newsapi.org/v2/everything?q=kansas+city&apiKey=331040a253684eed8346e5eb64aabfe0`) for all news
 fetch(`https://newsapi.org/v2/top-headlines?q=kansas+city&apiKey=331040a253684eed8346e5eb64aabfe0`) //for top headlines
 //fetch(`https://newsapi.org/v2/top-headlines?q=kansas+city&category=entertainment&apiKey=331040a253684eed8346e5eb64aabfe0`) //for top headlines for entertainment
 //replace "kansas+city" with " + var name for city input + ", pull city name from same input as weather?
             .then((response) => response.json())
-            .then(data => console.log(data)) 
+          .then(data => searchItem2.innerHTML=data.articles[0].description)
+//.then(data => console.log(data))
+
 
            // function error() {
                 //alert ('No results for that location. Please try again.')
